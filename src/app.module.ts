f51b1a9f1f230eccdc5cc43350b347177fb40c8f
senyameow <arseniyxd@gmail.com>
2023-12-19 23:49:01 +0700

    add get endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, OrdersModule],
+  imports: [UsersModule],
   controllers: [],
   providers: [],
 })
